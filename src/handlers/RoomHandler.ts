eb094787d1ef6fa4ce23c4ad98d1940ee87b646e
Saurav <palsaurav25@gmail.com>
2025-04-22 23:25:08 +0530

    Room With multiple people added

--- src/handlers/RoomHandler.ts
+++ src/handlers/RoomHandler.ts
@@ -1,19 +1,33 @@
 import { Socket } from "socket.io/dist";
 import {v4 as UUIDv4 } from "uuid";
+import IRoomParams from "../interfaces/IRoomParams";
+const rooms: Record<string, string[]> = {};
 
 const roomHandler = (socket: Socket) => {
+
 
     const createRoom = () => {
         // This is will be our unique room id for multiple connections establishments
         const roomId = UUIDv4();
         // Socket connection will join the created room
         socket.join(roomId);
+
+        rooms[roomId] = [];
         // An emit function from server side that server connection has been added to the room
         socket.emit("room-created", {roomId});
-        
+
     };
-    const joinedRoom = ({roomId}:{roomId:string}) => {
-         console.log("New user has Joined the room: ",roomId);
+    const joinedRoom = ({roomId, peerId}: IRoomParams) => {
+        if (rooms[roomId]) {
+            console.log("New user has Joined the room: ", roomId, " with peer ID as ", peerId);
+            rooms[roomId].push(peerId);
+            socket.join(roomId);
+            console.log("added in room", rooms);
+            socket.emit("get-users", {
+                roomId,
+                participants: rooms[roomId],
+            });
+        }
     };
 
     socket.on("create-room", createRoom);
